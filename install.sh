86f427567169113ff456ad49ca03663878f0a1b6
Administrator <gitlab_admin_9987b7@example.com>
2025-04-28 07:57:32 +0000

    Edit install.sh

--- install.sh
+++ install.sh
@@ -565,7 +565,7 @@ DownloadAndInstallCasaOS() {
     if [ -z "${BUILD_DIR}" ]; then
         ${sudo_cmd} rm -rf ${TMP_ROOT}
         ${sudo_cmd} mkdir -p ${TMP_ROOT} || Show 1 "Failed to create temporary directory"
-        TMP_DIR=$(${sudo_cmd} mktemp -d -p ${TMP_ROOT} || Show 1 "Failed to create temporary directory")
+        TMP_DIR=$(mktemp -d -p ${TMP_ROOT} || Show 1 "Failed to create temporary directory")
 
         pushd "${TMP_DIR}"
 
